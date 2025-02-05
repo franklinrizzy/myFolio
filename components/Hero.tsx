b38ff7b2b2674e2a6143dfd06672ef01701aa7cc
Rizwan Khan <franklinrizzy@gmail.com>
2025-02-06 02:14:46 +0530

    updated: hero page

---
+++ components/Hero.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Spotlight } from './ui/spotlight-new'
+import { TextGenerateEffect } from './ui/text-generate-effect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+        <div>
+            <Spotlight />
+            <Spotlight />
+        </div>
+        {/* Grid */}
+        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.05] bg-grid-black-100/[0.2]
+         flex items-center justify-center absolute top-0 left-0">
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
+        {/* text */}
+        <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Riz, a Web Developer.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
